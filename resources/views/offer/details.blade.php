794d9769abe00889267169d778f0dc3a24e66373
DudeSA <marcinsa1212@wp.pl>
2020-11-22 22:58:55 +0100

    frontend ofert

--- resources/views/offer/details.blade.php
+++ resources/views/offer/details.blade.php
@@ -1,9 +1,55 @@
 @extends('layouts.app')
 @section('content')
-    {!! $room->DescriptionLong !!}
-    {!! $room->RoomSpace !!}
-    {!! $room->PricePerDay !!}
-    @foreach($room->roomtags as $tag)
-    <p>{!! $tag->Name !!}</p>
-    @endforeach
+<section class="content">
+    <div class="row color h3 mb-0 p-3" >
+        <div class="text-white" style="border-bottom: 2px solid #fff; width: 100%">
+        {!! $room->DescriptionShort !!}
+        </div>
+    </div>
+    <div class="row color">
+        <div class="col-6 p-1 m-auto">
+            <img src="https://images.trvl-media.com/hotels/7000000/6370000/6361700/6361648/3b46cbf0.jpg?impolicy=fcrop&w=1200&h=800&p=1&q=medium" class="img-fluid">
+        </div>
+        <div class="col-3">
+            <div class="row pt-1 pr-1 pb-1">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Finteriordesignexplained.com%2Fimages%2Ffull-size%2F10%2Fcommunal-spaces-in-hotel-rooms-1.jpeg&f=1&nofb=1" class="img-fluid">
+            </div>
+            <div class="row pr-1 pb-1">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Finteriordesignexplained.com%2Fimages%2Ffull-size%2F10%2Fcommunal-spaces-in-hotel-rooms-1.jpeg&f=1&nofb=1" class="img-fluid">
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="row pt-1 pr-1 pb-1">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Finteriordesignexplained.com%2Fimages%2Ffull-size%2F10%2Fcommunal-spaces-in-hotel-rooms-1.jpeg&f=1&nofb=1" class="img-fluid">
+            </div>
+            <div class="row pr-1 pb-1">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Finteriordesignexplained.com%2Fimages%2Ffull-size%2F10%2Fcommunal-spaces-in-hotel-rooms-1.jpeg&f=1&nofb=1" class="img-fluid">
+            </div>
+        </div>
+    </div>
+    <div class="row shadow-lg">
+        <div class="col-9 pl-0 pt-3 pr-0 pb-3 text_justify" >
+            <div class="p-2" style="border-right: 4px solid #753c52; height: 100%">
+                {!! $room->DescriptionLong !!}
+            </div>
+        </div>
+        <div class="col-3 text-right pt-3">
+            <div class="h5 mb-0">
+                <strong>{!! $room->PricePerDay !!} zł</strong>
+            </div>
+            <div class="pt-0">
+                za noc
+            </div>
+            <div class="pb-3 pt-3 d-flex">
+                <div class="ml-auto d-flex">
+                    <img src="/svg/people.svg" style="max-height: 20px;">
+                    <div class="pl-1">{!! $room->RoomSpace !!} os.</div>
+                </div>
+            </div>
+            @foreach($room->roomtags as $tag)
+            {!! $tag->Name !!}
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
--- resources/views/offer/index.blade.php
+++ resources/views/offer/index.blade.php
@@ -14,8 +14,8 @@
         <div class="indi_room shadow-lg m-auto">
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Finteriordesignexplained.com%2Fimages%2Ffull-size%2F10%2Fcommunal-spaces-in-hotel-rooms-1.jpeg&f=1&nofb=1" class="room_img p-2 img-fluid">
             <div class="room_info mb-4 pb-2">
-                <div class="description ml-2">{!! $room->DescriptionShort !!}</div>
-                <div class="roomspace ml-2">Pokój dla {!! $room->RoomSpace !!} os.</div>
+                <h3 class="description ml-2">{!! $room->DescriptionShort !!}</h3>
+                <h5 class="roomspace ml-2">Pokój dla {!! $room->RoomSpace !!} os.</h5>
                 <div class="row px-4">
                     <div>
                         <button type="button" class="btn btn-default priceperday"><strong style="padding-right:5px;">{!! $room->PricePerDay !!} zł</strong>/noc</button>
